f4b2b6178762efcd4f5d830564b4f8c8ba2390b8
Kini99 <kinjalmomaya99@gmail.com>
2025-08-19 18:15:46 +0530

    feat(rag): update langgraph workflow

--- backend/rag/retriever.py
+++ backend/rag/retriever.py
@@ -2,12 +2,15 @@ import getpass
 import os
 from langchain.chat_models import init_chat_model
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain_chroma import Chroma
+from langchain_community.vectorstores import Chroma
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import CSVLoader, PyPDFLoader
-from langchain_core.documents import Document
-from langgraph.graph import START, StateGraph
-from typing_extensions import List, TypedDict
+from langgraph.graph import  END, StateGraph, MessagesState
+from langchain_core.tools import tool
+from langchain_core.messages import SystemMessage
+from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.checkpoint.memory import MemorySaver
+
 import chromadb
 
 client = chromadb.Client()
@@ -24,11 +27,11 @@ vector_store = Chroma(
     persist_directory="./chroma_db", 
 )
 
-loader1 =CSVLoader("./docs/exercise.csv")
+loader1 =CSVLoader("../docs/exercise.csv")
 docs1 = loader1.load()
-loader2 = CSVLoader("./docs/nutrition.csv")
+loader2 = CSVLoader("../docs/nutrition.csv")
 docs2 = loader2.load()
-loader3 = PyPDFLoader("./docs/fitness.pdf")
+loader3 = PyPDFLoader("../docs/fitness.pdf")
 docs3 = loader3.load()
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
@@ -40,35 +43,73 @@ _ = vector_store.add_documents(documents=all_splits_exercise)
 _ = vector_store.add_documents(documents=all_splits_nutrition)
 _ = vector_store.add_documents(documents=all_splits_fitness)
 
-prompt=f"""You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
-Question: {question} 
-Context: {context} 
-Answer:"""
+graph_builder = StateGraph(MessagesState)
 
-# Define state for application
-class State(TypedDict):
-    question: str
-    context: List[Document]
-    answer: str
+@tool(response_format="content_and_artifact")
+def retrieve(query: str):
+    """Retrieve information related to a query."""
+    retrieved_docs = vector_store.similarity_search(query, k=2)
+    serialized = "\n\n".join(
+        (f"Content: {doc.page_content}")
+        for doc in retrieved_docs
+    )
+    return serialized, retrieved_docs
 
-
-# Define application steps
-def retrieve(state: State):
-    retrieved_docs = vector_store.similarity_search(state["question"])
-    return {"context": retrieved_docs}
-
-
-def generate(state: State):
-    docs_content = "\n\n".join(doc.page_content for doc in state["context"])
-    messages = prompt.invoke({"question": state["question"], "context": docs_content})
-    response = llm.invoke(messages)
-    return {"answer": response.content}
+# Step 1: Generate an AIMessage that may include a tool-call to be sent.
+def query_or_respond(state: MessagesState):
+    """Generate tool call for retrieval or respond."""
+    llm_with_tools = llm.bind_tools([retrieve])
+    response = llm_with_tools.invoke(state["messages"])
+    return {"messages": [response]}
 
 
-# Compile application and test
-graph_builder = StateGraph(State).add_sequence([retrieve, generate])
-graph_builder.add_edge(START, "retrieve")
-graph = graph_builder.compile()
+# Step 2: Execute the retrieval.
+tools = ToolNode([retrieve])
 
-response = graph.invoke({"question": "What is Task Decomposition?"})
-print(response["answer"])
+# Step 3: Generate a response using the retrieved content.
+def generate(state: MessagesState):
+    """Generate answer."""
+    recent_tool_messages = []
+    for message in reversed(state["messages"]):
+        if message.type == "tool":
+            recent_tool_messages.append(message)
+        else:
+            break
+    tool_messages = recent_tool_messages[::-1]
+
+    docs_content = "\n\n".join(doc.content for doc in tool_messages)
+    system_message_content = (
+        f"""You are an assistant for question-answering tasks. 
+        Use the following pieces of retrieved context to answer 
+        the question. If you don't know the answer, say that you 
+        don't know. Keep the answer concise.
+        \n\n
+        {docs_content}"""
+    )
+    conversation_messages = [
+        message
+        for message in state["messages"]
+        if message.type in ("human", "system")
+        or (message.type == "ai" and not message.tool_calls)
+    ]
+    prompt = [SystemMessage(system_message_content)] + conversation_messages
+
+    response = llm.invoke(prompt)
+    return {"messages": [response]}
+
+graph_builder.add_node(query_or_respond)
+graph_builder.add_node(tools)
+graph_builder.add_node(generate)
+
+graph_builder.set_entry_point("query_or_respond")
+graph_builder.add_conditional_edges(
+    "query_or_respond",
+    tools_condition,
+    {END: END, "tools": "tools"},
+)
+graph_builder.add_edge("tools", "generate")
+graph_builder.add_edge("generate", END)
+
+memory = MemorySaver()
+graph = graph_builder.compile(checkpointer=memory)
+config = {"configurable": {"thread_id": "abc123"}}
